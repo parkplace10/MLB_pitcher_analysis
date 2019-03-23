bb7a213cf46da6cc2bf48303caef6207319bdbd7
Robert Voss <bob.voss.21@gmail.com>
2019-03-23 01:24:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,104 +1,35 @@
 import os
-
-import pandas as pd
+import io
 import numpy as np
 
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-
-from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, redirect, url_for, jsonify, render_template
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def home_page():
+    if request.method == 'POST':
+        # read the request data into our variables
 
-#################################################
-# Database Setup
-#################################################
+        # https://stackoverflow.com/questions/10434599/how-to-get-data-received-in-flask-request
+        # use 'get' if needed
+        pitcher = request.form['pitcher']
+        
+    else:
+        # put the default data into our variables
+        pitcher = 'Adam Wainwright'
+        
 
-#CHANGE app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
-db = SQLAlchemy(app)
+    # take this input - load the right model based on the pitcher selected
+    # feed the rest of the variables into model.predict(...)
+    # 
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
+    input_data = { 'pitcher': pitcher }
+    output_data = None
 
-# Save references to each table
-# CHANGE Samples_Metadata = Base.classes.sample_metadata
-# CHANGE Samples = Base.classes.samples
-
-
-@app.route("/")
-def index():
-    """Return the homepage."""
-    return render_template("index-2.html")
-
-
-# @app.route("/names")
-# def names():
-#     """Return a list of sample names."""
-
-#     # Use Pandas to perform the sql query
-#     stmt = db.session.query(Samples).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
-
-#     # Return a list of the column names (sample names)
-#     return jsonify(list(df.columns)[2:])
-
-
-# @app.route("/metadata/<sample>")
-# def sample_metadata(sample):
-#     """Return the MetaData for a given sample."""
-#     sel = [
-#         Samples_Metadata.sample,
-#         Samples_Metadata.ETHNICITY,
-#         Samples_Metadata.GENDER,
-#         Samples_Metadata.AGE,
-#         Samples_Metadata.LOCATION,
-#         Samples_Metadata.BBTYPE,
-#         Samples_Metadata.WFREQ,
-#     ]
-
-#     results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
-
-#     # Create a dictionary entry for each row of metadata information
-#     sample_metadata = {}
-#     for result in results:
-#         sample_metadata["sample"] = result[0]
-#         sample_metadata["ETHNICITY"] = result[1]
-#         sample_metadata["GENDER"] = result[2]
-#         sample_metadata["AGE"] = result[3]
-#         sample_metadata["LOCATION"] = result[4]
-#         sample_metadata["BBTYPE"] = result[5]
-#         sample_metadata["WFREQ"] = result[6]
-
-#     print(sample_metadata)
-#     return jsonify(sample_metadata)
-
-
-# @app.route("/samples/<sample>")
-# def samples(sample):
-#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-#     stmt = db.session.query(Samples).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
-
-#     # Filter the data based on the sample number and
-#     # only keep rows with values above 1
-#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-
-#     # Sort by sample
-#     sample_data.sort_values(by=sample, ascending=False, inplace=True)
-
-#     # Format the data to send as json
-#     data = {
-#         "otu_ids": sample_data.otu_id.values.tolist(),
-#         "sample_values": sample_data[sample].values.tolist(),
-#         "otu_labels": sample_data.otu_label.tolist(),
-#     }
-#     return jsonify(data)
+    # would probably want to put stuff into dicts
+    return render_template("index.html", input_data=input_data, output_data=output_data)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
