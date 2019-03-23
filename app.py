21053b225b2928fc6efc90aa5fedad48eae0ef4b
Robert Voss <bob.voss.21@gmail.com>
2019-03-23 11:14:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,18 +14,32 @@ def home_page():
         # https://stackoverflow.com/questions/10434599/how-to-get-data-received-in-flask-request
         # use 'get' if needed
         pitcher = request.form['pitcher']
+        inning = int(request.form['inning'])
+        outs = int(request.form['outs'])
+        balls = int(request.form['balls'])
+        strikes = int(request.form['strikes'])
+        stance = request.form['stance']
+        first = request.form['first']
+        second = request.form['second']
+        third = request.form['third']
+        pitch_type = ""
+        pitch_location = ""
+
         
     else:
         # put the default data into our variables
         pitcher = 'Adam Wainwright'
+        pitch_type = 'Four-Seamer'
+        pitch_location = 'middle center'
         
 
     # take this input - load the right model based on the pitcher selected
     # feed the rest of the variables into model.predict(...)
     # 
 
-    input_data = { 'pitcher': pitcher }
-    output_data = None
+    input_data = { 'pitcher': pitcher} #'inning': inning, 'outs':outs, 'balls':balls, 'strikes':strikes, 
+    #'stance':stance, 'first':first, 'second':second, 'third':third
+    output_data = None #{'pitch type': pitch_type, 'pitch location': pitch_location}
 
     # would probably want to put stuff into dicts
     return render_template("index.html", input_data=input_data, output_data=output_data)
