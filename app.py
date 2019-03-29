94f76991c3e5848a4f0e8677e421e8ffaf843f60
Robert Voss <bob.voss.21@gmail.com>
2019-03-28 20:27:50 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
 import io
 import numpy as np
+import pandas as import pd
+import pickle
 from flask import Flask, request, redirect, url_for, jsonify, render_template
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
 
 
 app = Flask(__name__)
@@ -27,34 +25,35 @@ def home_page():
         strikes = float(request.form['strikes'])
         pitch_num = float(request.form['pitch_num'])
         stance = request.form['stance']
-        first = request.form['first']
-        second = request.form['second']
-        third = request.form['third']
-        previous_pitch_type = ""
-        previous_pitch_location = ""
+        previous_pitch_type = request.form['previous_pitch_type']
+        previous_pitch_location = request.form['previous_pitch_location']
 
-        # new = np.array(['outs', 'balls', ''strikes])
+        new = np.array([pitcher, inning, pscore, bscore, outs, balls, strikes, pitch_num, stance, previous_pitch_type, previous_pitch_location])
 
-        # model.predict(new)
+        pitch_type_model = pickle.load(open('models/pitch_type_model.sav', 'rb'))
+
+        pitch_location_model = pickle.load(open('models/pitch_location_model.sav', 'rb'))
+
+        predicted_pitch_type = pitch_type_model.predict(new)
+        predicted_pitch_location = pitch_location_model.predict(new)
         
     else:
         # put the default data into our variables
         pitcher = 'Adam Wainwright'
-        pitch_type = 'Four-Seamer'
-        pitch_location = 'middle center'
+        predicted_pitch_type = 'Four-Seamer'
+        predicted_pitch_location = 'middle center'
         
 
-    # take this input - load the right model based on the pitcher selected
-    # feed the rest of the variables into model.predict(...)
-    # 
+    input_data = { 'pitcher': pitcher, 'inning': inning, 'outs':outs, 'balls':balls, 'strikes':strikes, 'pitch_num':pitch_num, 'stance':stance, 'previous_pitch_type':previous_pitch_type, 'previous_pitch_location': previous_pitch_location}
+    output_data = {'predicted pitch type': predicted_pitch_type, 'predicted pitch location': predicted_pitch_location}
 
-    input_data = { 'pitcher': pitcher, 'inning': inning, 'outs':outs, 'balls':balls, 'strikes':strikes, 
-    'pitch_num':pitch_num, 'stance':stance, 'first':first, 'second':second, 'third':third}
-    output_data = None #{'pitch type': pitch_type, 'pitch location': pitch_location}
-
-    # would probably want to put stuff into dicts
+    
     return render_template("index-2.html", input_data=input_data, output_data=output_data)
 
+@app.route('/dash')
+def dash():
+
+    return render_template("index-3.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
