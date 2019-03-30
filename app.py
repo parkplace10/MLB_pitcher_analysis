433214f634de4d5535ce7eea20d8da6bf4b1c6f1
Robert Voss <bob.voss.21@gmail.com>
2019-03-30 04:26:34 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ import io
 import numpy as np
 import pandas as pd
 import pickle
+import sklearn
 from flask import Flask, request, redirect, url_for, jsonify, render_template
 
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
     
     if request.method == 'POST':
+
+        
         # read the request data into our variables
 
         # https://stackoverflow.com/questions/10434599/how-to-get-data-received-in-flask-request
@@ -24,15 +28,15 @@ def home_page():
         outs = float(request.form['outs'])
         balls = float(request.form['balls'])
         strikes = float(request.form['strikes'])
-        pitch_num = float(request.form['pitch_num'])
+        pitchnum = float(request.form['pitchnum'])
         stance = request.form['stance']
+        typeloc = request.form['typeloc']
         previous_pitch_type = request.form['previous_pitch_type']
         previous_pitch_location = request.form['previous_pitch_location']
 
-        new = np.array([[pitcher, inning, pscore, bscore, outs, balls, strikes, pitch_num, stance, previous_pitch_type, previous_pitch_location]])
+        new = np.array([[pitcher, inning, pscore, bscore, outs, balls, strikes, pitchnum, stance, typeloc, previous_pitch_type, previous_pitch_location]])
 
         pitch_type_model = pickle.load(open('models/pitch_type_model.sav', 'rb'))
-
         pitch_location_model = pickle.load(open('models/pitch_location_model.sav', 'rb'))
 
         predicted_pitch_type = pitch_type_model.predict(new)
@@ -45,20 +49,22 @@ def home_page():
         outs = '0'
         balls = '0'
         strikes = '0'
-        pitch_num = '1'
+        pitchnum = '1'
         stance = '1'
+        typeloc = '1'
+        pscore = '1'
+        bscore = '1'
         previous_pitch_type = '1'
         previous_pitch_location = '1'
         predicted_pitch_type = '1'
         predicted_pitch_location = '1'
         
 
-    input_data = { 'pitcher': pitcher, 'inning': inning, 'outs':outs, 'balls':balls, 'strikes':strikes, 'pitch_num':pitch_num, 'stance':stance, 
-    'previous_pitch_type':previous_pitch_type, 'previous_pitch_location': previous_pitch_location}
+    input_data = { 'pitcher': pitcher, 'inning': inning, 'outs':outs, 'balls':balls, 'strikes':strikes, 'pitchnum':pitchnum, 'stance':stance, 'typeloc':typeloc, 'pscore':pscore, 'bscore':bscore, 'previous_pitch_type':previous_pitch_type, 'previous_pitch_location': previous_pitch_location}
     output_data = {'predicted pitch type': predicted_pitch_type, 'predicted pitch location': predicted_pitch_location}
 
     
-    return render_template("index-2.html", input_data=input_data, output_data=output_data)
+    return render_template("index.html", input_data=input_data, output_data=output_data)
 
 @app.route('/dash')
 def dash():
